c9933ba5096a2f95bd650a6801a1e5e9bed8692f
Ambika Sukla <ambikasukla@Ambikas-MacBook-Pro.local>
2023-10-17 19:50:13 -0400

    used new release number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='llmsherpa',
-    version='0.1.0',    
+    version='0.1.1',    
     description='Strategic APIs to Accelerate LLM Use Cases',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
